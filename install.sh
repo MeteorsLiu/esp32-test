e1e989826a503910e1810968709015f9baec057e
Haolan <hello@liuxi.ng>
2025-08-19 15:20:56 +0800

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
- python /Users/haolan/esp/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 elf2image --flash_mode dio --flash_freq 40m --flash_size 2MB --min-rev-full 0 --max-rev-full 399 -o t.bin t.elf
+ llgo build -target esp32 -v -tags nogc -o t.elf
+ python -m esptool --chip esp32 elf2image --flash_mode dio --flash_freq 40m --flash_size 2MB --min-rev-full 0 --max-rev-full 399 -o t.bin t.elf
  python -m esptool --chip esp32 -b 460800 --before default_reset --after hard_reset write_flash --flash_mode dio --flash_size 2MB --flash_freq 40m 0x1000 t.bin
